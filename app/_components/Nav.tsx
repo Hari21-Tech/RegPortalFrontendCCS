522ef11d32247ccf0bbf4328d99902e214c1c66b
Devyang Sharma <devyangthunder@gmail.com>
2025-07-09 15:43:10 +0530

    basics done

---
+++ app/_components/Nav.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav: React.FC = () => {
+  return (
+    <nav className="flex justify-start p-4 w-full">
+      <img src="/ccs_logo.png" />
+    </nav>
+  );
+};
+
+export default Nav;
