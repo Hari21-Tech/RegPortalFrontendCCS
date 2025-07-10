e416fcdd5895c171eb2f4a98c6a5f22cd01297b4
sz05 <Shreyans0528@gmail.com>
2025-07-10 08:38:01 +0530

    /register bg done

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button";
 import React from "react";
-import Nav from "../_components/Nav";
+import Bg from "./components/Bg";
+// import Nav from "../_components/Nav";
 
 export default function RegisterPage() {
   return (
     <div className="relative h-screen flex flex-col items-center">
-      <div className="absolute inset-0 md:bg-[url('/Vector.png')] bg-[url('/vector2.png')] bg-cover bg-center bg-no-repeat -z-10  " />
-      <Nav />
+      {/* <div className="absolute inset-0 md:bg-[url('/Vector.png')] bg-[url('/vector2.png')] bg-cover bg-center bg-no-repeat -z-10  " /> */}
+      {/* <Nav /> */}
+      <Bg />
 
       <div className="absolute bottom-1 flex flex-col space-y-5 min-w-96 pb-20 ">
         <Button size={"xl"}>Join Team</Button>
